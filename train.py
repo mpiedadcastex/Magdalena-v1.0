785addadbd24acd840d4cbb6c06e5a223cdc06dd
Mapi Castex <mapicastex@gmail.com>
2025-12-24 19:45:15 +0100

    Correción de rutas para Colab

--- train.py
+++ train.py
@@ -7,10 +7,17 @@ from src.data.maestro_dataset import get_dataloaders
 ap = AudioProcessor(fmax=8000, n_mels=229) 
 mp = MidiProcessor() # Tus parámetros de MIDI
 
+
 # 2. Obtener los loaders pasando los objetos 'ap' y 'mp'
 train_loader, val_loader = get_dataloaders(
-    csv_path='src/data/maestro-v3.0.0_metadata.csv',
-    root_dir='src/data/raw/maestro-v3.0.0/maestro-v3.0.0',
+    # Para ejecución en Colab
+    csv_path='/content/drive/MyDrive/TFG_Data/maestro-v3.0.0/',
+    root_dir= '/content/drive/MyDrive/TFG_Data/maestro-v3.0.0/maestro-v3.0.0',
+
+    # Para ejecución en local
+    #csv_path='data/maestro-v3.0.0_metadata.csv',
+    #root_dir='data/raw/maestro-v3.0.0/maestro-v3.0.0'
+
     audio_processor=ap,  # <--- Inyección de dependencia
     midi_processor=mp,
     batch_size=4
