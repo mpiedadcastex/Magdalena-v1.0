6df3d3a3133cdec3a1d8a8c86c23a5bbbcb23760
Mapi <mapicastex@gmail.com>
2026-04-05 14:42:58 +0200

    Corrección de formato de ruta

--- evaluate.py
+++ evaluate.py
@@ -15,7 +15,7 @@ from utils import get_model_config
 # Configuración
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 EPOCHS_COMPLETED = 100
-CHECKPOINT_PATH = "/content/drive/MyDrive/TFG_Project/MPCS/checkpoints/model/model_epoch_{EPOCHS_COMPLETED}.pth"    # Ajusta a la última versión del entrenamiento
+CHECKPOINT_PATH = f"/content/drive/MyDrive/TFG_Project/MPCS/checkpoints/model/model_epoch_{EPOCHS_COMPLETED}.pth"    # Ajusta a la última versión del entrenamiento
 CSV_PATH = '/content/drive/MyDrive/TFG_Data/maestro-v3.0.0/maestro-v3.0.0_metadata.csv'
 ROOT_DIR = '/content/drive/MyDrive/TFG_Data/maestro-v3.0.0/maestro-v3.0.0'
 OUTPUT_DIR = "evaluation_results"
